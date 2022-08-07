ffe9d3cb843b79574cf162af7beff7a86d546e59
Rico Ferdian <ferdianrico6@gmail.com>
2022-08-08 01:22:57 +0700

    menyederhanakan dashboard admin

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -2,9 +2,18 @@
 	<div class="container-fluid">
 		<!-- Page Heading -->
 		<h1 class="h3 mb-4 text-gray-800"><?= $title ?></h1>
+		<?php 
+		$dashboard = $this->db->get('dashboard')->row_array(1);
+		?>
 		<div class="card text-left">
 			<div class="card-header">
 				<?= $dashboard['header'] ?>
+				<?php 
+				$role_id = $this->session->userdata('role_id'); 
+				$queryMenu = "SELECT um.id, menu FROM user_menu AS um JOIN user_access_menu AS uam ON um.id = uam.menu_id WHERE uam.role_id = $role_id AND active = 1 ORDER BY uam.menu_id ASC";
+				$menu = $this->db->query($queryMenu)->result_array();
+				var_dump($menu);
+ ?>
 			</div>
 			<div class="card-body">
 				<h5 class="card-title"><?= $dashboard['title'] ?></h5>
